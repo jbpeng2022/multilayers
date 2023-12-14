0df6ffdaab937b99ae388998d96671a78568fbc0
jbpeng2022 <104357372+jbpeng2022@users.noreply.github.com>
2023-12-14 19:27:21 +0800

    Delete interface_t.m

--- interface_t.m
+++
@@ -1,6 +0,0 @@
-function y=interface_t(pol, n_i, n_f, th_i, th_f)
-if pol == 's'
-    y=2 * n_i * cos(th_i) / (n_i * cos(th_i) + n_f * cos(th_f));
-elseif pol == 'p'
-    y=2 * n_i * cos(th_i) / (n_f * cos(th_i) + n_i * cos(th_f));
-end
