76396c180525b2f0d903e91459484a1c8dc70a17
jbpeng2022 <104357372+jbpeng2022@users.noreply.github.com>
2024-03-10 20:22:24 +0800

    Delete tmm directory

--- tmm/interface_r.m
+++
@@ -1,7 +0,0 @@
-function y=interface_r(pol,n_i,n_f,th_i,th_f)
-if pol=='s'
-    y=(n_i * cos(th_i) - n_f * cos(th_f)) /(n_i * cos(th_i) + n_f * cos(th_f));
-elseif pol=='p'
-    y=(n_f * cos(th_i) - n_i * cos(th_f)) /(n_f * cos(th_i) + n_i * cos(th_f));
-end
-end
